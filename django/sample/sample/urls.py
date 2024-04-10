1d15bb97d986ce757bb23febf88364d3affff505
cocoajoa <cocoajoa12@gmail.com>
2024-04-11 00:58:42 +0900

    2024-04-11 1st

--- django/sample/sample/urls.py
+++ django/sample/sample/urls.py
@@ -23,4 +23,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('articles/', include('articles.urls')),
+    path('main/', include('main.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
